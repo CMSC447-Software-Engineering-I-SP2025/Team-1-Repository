2a4ad62b0c6a4580ae351515bb5e3876909c4133
Michael Rohde <52460899+xierkov@users.noreply.github.com>
2025-03-08 04:04:36 -0500

    Test controller with invalid states

--- Backend/BoulderBuddyAPI.Tests/Controllers/SearchControllerTests.cs
+++ Backend/BoulderBuddyAPI.Tests/Controllers/SearchControllerTests.cs
@@ -1,9 +1,12 @@
 ﻿using BoulderBuddyAPI.Controllers;
+using BoulderBuddyAPI.Models;
 using BoulderBuddyAPI.Models.OpenBetaModels;
 using BoulderBuddyAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Moq.Protected;
+using RichardSzalay.MockHttp;
 using System.Text.Json;
 
 namespace BoulderBuddyAPI.Tests.Controllers
@@ -11,7 +14,7 @@ namespace BoulderBuddyAPI.Tests.Controllers
     public class SearchControllerTests
     {
         [Fact]
-        public async Task SearchByLocationState_GivenValidState_ReturnsListOfSubareas()
+        public async Task SearchByLocationState_GivenValidState_Returns200OkWithListOfSubareas()
         {
             //read subareas as OpenBetaQueryService would, but without actually pinging OpenBeta API
             var jsonString = File.ReadAllText("TestResources/ClimbsByStateResponse.json");
@@ -38,6 +41,37 @@ namespace BoulderBuddyAPI.Tests.Controllers
 
             //deep equality by objects' public properties
             Assert.Equivalent(expectedReturn, resultEnumerable);
+        }
+
+        [Fact]
+        public async Task SearchByLocationState_GivenInvalidState_Returns400BadRequestWithErrorMsg()
+        {
+            //setup OpenBetaQueryService with mocked HttpClient (so it doesn't actually ping OpenBeta)
+            var mockOBQSLogger = new Mock<ILogger<OpenBetaQueryService>>();
+            var config = new OpenBetaConfig()
+            {
+                OpenBetaEndpoint = "UNUSED",
+                SupportedRootAreas = ["Delaware", "Maryland"]
+            };
+            var mockHttpMsgHandler = new MockHttpMessageHandler();
+            var injectedHttpClient = new HttpClient(mockHttpMsgHandler);
+            var openBetaQueryService = new OpenBetaQueryService(mockOBQSLogger.Object, injectedHttpClient, config);
+
+            //create mock controller
+            var mockLogger = new Mock<ILogger<SearchController>>();
+            var controller = new SearchController(mockLogger.Object, openBetaQueryService);
+
+            string[] invalidStatesToTry = ["Mississippi", "Hawaii", "", "DELAWARE", null];
+            foreach (var invalid in invalidStatesToTry)
+            {
+                var result = await controller.SearchByLocation(invalid); //act
+
+                Assert.IsType<BadRequestObjectResult>(result);
+                var resultAsObjectResult = (BadRequestObjectResult)result;
+
+                var errMsg = resultAsObjectResult.Value;
+                Assert.IsType<string>(errMsg);
+            }
         }
     }
 }
