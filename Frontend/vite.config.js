4c79ac0bec18e9b16302901b94b10ab82d4fd51b
Michael Rohde <52460899+xierkov@users.noreply.github.com>
2025-03-11 22:08:14 -0400

    Allow CORS requests from frontend to backend

---
+++ Frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import plugin from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [plugin()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+      proxy: {
+        // http://localhost:5173/Search
+        //   -> http://localhost:5091/Search
+        '/Search': {
+            target: 'http://localhost:5091',
+            changeOrigin: true,
+        },
+       }
+    }
+})
