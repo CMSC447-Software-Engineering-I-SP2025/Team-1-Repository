9475ee2fe990f25bc297c1aed089c6c0a6c50af1
Michael Rohde <52460899+xierkov@users.noreply.github.com>
2025-04-09 04:21:37 -0400

    Filter options should be optional

--- Backend/BoulderBuddyAPI/Models/SearchWithFiltersOptions.cs
+++ Backend/BoulderBuddyAPI/Models/SearchWithFiltersOptions.cs
@@ -3,20 +3,20 @@ namespace BoulderBuddyAPI.Models
     public class SearchWithFiltersOptions
     {
         public string State { get; set; }
-        public DistanceFromCenterOptions DistOptions { get; set; }
-        public string MinBrazilianCrux { get; set; }
-        public string MaxBrazilianCrux { get; set; }
-        public string MinEwbank { get; set; }
-        public string MaxEwbank { get; set; }
-        public string MinFont { get; set; }
-        public string MaxFont { get; set; }
-        public string MinFrench { get; set; }
-        public string MaxFrench { get; set; }
-        public string MinUIAA { get; set; }
-        public string MaxUIAA { get; set; }
-        public string MinVscale { get; set; }
-        public string MaxVscale { get; set; }
-        public string MinYDS { get; set; }
-        public string MaxYDS { get; set; }
+        public DistanceFromCenterOptions? DistOptions { get; set; }
+        public string? MinBrazilianCrux { get; set; }
+        public string? MaxBrazilianCrux { get; set; }
+        public string? MinEwbank { get; set; }
+        public string? MaxEwbank { get; set; }
+        public string? MinFont { get; set; }
+        public string? MaxFont { get; set; }
+        public string? MinFrench { get; set; }
+        public string? MaxFrench { get; set; }
+        public string? MinUIAA { get; set; }
+        public string? MaxUIAA { get; set; }
+        public string? MinVscale { get; set; }
+        public string? MaxVscale { get; set; }
+        public string? MinYDS { get; set; }
+        public string? MaxYDS { get; set; }
     }
 }
