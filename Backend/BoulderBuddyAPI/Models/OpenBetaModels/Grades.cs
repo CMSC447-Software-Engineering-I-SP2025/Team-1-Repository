45103aeeafe86edbf3b78c213f59947bdd021b7a
Michael Rohde <52460899+xierkov@users.noreply.github.com>
2025-04-09 04:34:51 -0400

    Removed three unused grade types in the USA

--- Backend/BoulderBuddyAPI/Models/OpenBetaModels/Grades.cs
+++ Backend/BoulderBuddyAPI/Models/OpenBetaModels/Grades.cs
@@ -2,11 +2,8 @@
 {
     public class Grades
     {
-        public object brazilianCrux { get; set; }
-        public object ewbank { get; set; }
         public string font { get; set; }
         public string french { get; set; }
-        public object uiaa { get; set; }
         public string vscale { get; set; }
         public string yds { get; set; }
     }
